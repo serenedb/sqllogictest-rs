f23368af7759101156c5a054d542a56756efd46f
Mikhail Kornaukhov <mkornaukhov@serenedb.com>
2025-10-06 13:57:59 +0200

    test

--- sqllogictest-bin/src/main.rs
+++ sqllogictest-bin/src/main.rs
@@ -217,6 +217,8 @@ fn import_partition_config_from_ci() {
 pub async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
+    println!("Hello, world!");
+
     import_partition_config_from_ci();
 
     let cli = Opt::command().disable_help_flag(true).arg(
