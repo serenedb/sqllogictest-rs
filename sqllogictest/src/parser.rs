f00555bf07f800afc09d8d6a1fcf212a6b538909
Mikhail Kornaukhov <mkornaukhov@serenedb.com>
2025-12-02 11:25:58 +0100

    better

--- sqllogictest/src/parser.rs
+++ sqllogictest/src/parser.rs
@@ -188,8 +188,13 @@ pub enum Record<T: ColumnType> {
         loc: Location,
         threshold: u64,
     },
-    /// Show column names or not. True by default
+    /// Show column names or not. True by default.
     ShowColumnNames {
+        loc: Location,
+        value: bool,
+    },
+    /// All values in single column.
+    FlatValues {
         loc: Location,
         value: bool,
     },
@@ -360,6 +365,9 @@ impl<T: ColumnType> std::fmt::Display for Record<T> {
             }
             Record::ShowColumnNames { loc: _, value } => {
                 write!(f, "show-column-names {value}")
+            }
+            Record::FlatValues { loc: _, value } => {
+                write!(f, "flat-values {value}")
             }
             Record::Comment(comment) => {
                 let mut iter = comment.iter();
@@ -976,6 +984,12 @@ fn parse_inner<T: ColumnType>(loc: &Location, script: &str) -> Result<Vec<Record
                     .parse::<bool>()
                     .map_err(|_| ParseErrorKind::InvalidBool((*value).into()).at(loc.clone()))?,
             }),
+            ["flat-values", value] => records.push(Record::FlatValues {
+                loc: loc.clone(),
+                value: value
+                    .parse::<bool>()
+                    .map_err(|_| ParseErrorKind::InvalidBool((*value).into()).at(loc.clone()))?,
+            }),
             _ => return Err(ParseErrorKind::InvalidLine(line.into()).at(loc)),
         }
     }
@@ -1318,6 +1332,7 @@ select * from foo;
                     Record::Halt { loc, .. } => normalize_loc(loc),
                     Record::HashThreshold { loc, .. } => normalize_loc(loc),
                     Record::ShowColumnNames { loc, .. } => normalize_loc(loc),
+                    Record::FlatValues { loc, .. } => normalize_loc(loc),
                     // even though these variants don't include a
                     // location include them in this match statement
                     // so if new variants are added, this match
